31824970bdf7f2f3276654bf9f85988d44f3130c
mercy chepkemoi koech <koechmercyk@gmail.com>
2025-05-08 21:01:05 +0300

    sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    #pass
+    fib_sequence =[]
+    
+   
+    for i in range(length):
+        if i == 0:
+            fib_sequence.append(0)
+        elif i == 1:
+            fib_sequence.append(1)
+        else:
+            fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
+    
+    print(fib_sequence)       
